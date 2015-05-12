e76029b0761ae2ce6ca761b9b7f5eb5474cb074a
berrabahs <sofiane_berrabah@hotmail.fr>
2015-05-12 15:10:08 +0200

    conflict

--- Symfony/src/AppBundle/Controller/UserController.php
+++ Symfony/src/AppBundle/Controller/UserController.php
@@ -75,27 +75,28 @@ class UserController extends Controller
     }*/
 
     /**
-     * @Route("/login", name="login")
-     */
-    public function loginAction(Request $request)
-    {
-        $authenticationUtils = $this->get('security.authentication_utils');
+        *@Route("login", name="login")
+        */
+        public function loginAction(Request $request){
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+             $authenticationUtils = $this->get('security.authentication_utils');
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
 
-        return $this->render(
-            'user/login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            )
-        );
-    }
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+
+            return $this->render(
+                'user/login.html.twig',
+                array(
+                    // last username entered by the user
+                    'last_username' => $lastUsername,
+                    'error'         => $error,
+                )
+            );
+        }
     /**
      * @Route("/login_check", name="login_check")
      */
