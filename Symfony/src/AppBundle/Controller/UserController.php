45be20587079fb5cbde1d57062cde1770231a853
darkontes1 <darkontes@hotmail.fr>
2015-05-12 15:33:31 +0200

    fu security

--- Symfony/src/AppBundle/Controller/UserController.php
+++ Symfony/src/AppBundle/Controller/UserController.php
@@ -75,28 +75,28 @@ class UserController extends Controller
     }*/
 
     /**
-        *@Route("login", name="login")
-        */
-        public function loginAction(Request $request){
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request){
 
-             $authenticationUtils = $this->get('security.authentication_utils');
+        $authenticationUtils = $this->get('security.authentication_utils');
 
-            // get the login error if there is one
-            $error = $authenticationUtils->getLastAuthenticationError();
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
-            // last username entered by the user
-            $lastUsername = $authenticationUtils->getLastUsername();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
 
-            return $this->render(
-                'user/login.html.twig',
-                array(
-                    // last username entered by the user
-                    'last_username' => $lastUsername,
-                    'error'         => $error,
-                )
-            );
-        }
+        return $this->render(
+            'user/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
     /**
      * @Route("/login_check", name="login_check")
      */
