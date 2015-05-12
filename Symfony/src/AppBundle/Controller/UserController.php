409a8dc7d261b8fe1c09ef832c666f69aa07e311
berrabahs <sofiane_berrabah@hotmail.fr>
2015-05-12 14:29:54 +0200

    sert

--- Symfony/src/AppBundle/Controller/UserController.php
+++ Symfony/src/AppBundle/Controller/UserController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Util\SecureRandom;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
