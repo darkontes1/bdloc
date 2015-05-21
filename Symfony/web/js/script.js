b15a06b0b32d54832655da2fa889978d32e75d6c
berrabahs <sofiane_berrabah@hotmail.fr>
2015-05-21 10:42:23 +0200

    dgfdg

--- Symfony/web/js/script.js
+++ Symfony/web/js/script.js
@@ -2,6 +2,26 @@ $(document).on("click",".ajoutPanier",function(e){
     //console.log("aer");
     var button = $(this).attr("data-bdid");
     //var panier = $("#panier").val();
+    var dispo = ($(".dispo").html());
+    $.ajax({
+        method:"POST",
+        url:button,
+        data:{"dispo":dispo
+        },
+        success:function(r){
+            //console.log(r);
+            dispo -= 1;
+            //$(this).parents(".dispo").html("toto");
+            //$(this).attr(".dispo").text($(this).attr(".dispo").text()-1);
+        }
+    });
+});
+
+$(document).on("click",".panier_delete",function(e){
+    console.log("aer");
+    $(".test").hide();
+    var button = $(this).attr("data-remove");
+    //var panier = $("#panier").val();
     var dispo = ($(".dispo").val());
     $.ajax({
         method:"POST",
@@ -10,19 +30,6 @@ $(document).on("click",".ajoutPanier",function(e){
         },
         success:function(r){
             //console.log(r);
-            dispo -= 1;
-        }
-    });
-    $.ajax({
-        method:"GET",
-        url:button,
-        data:{
-            "dispo":dispo
-        },
-        success:function(r){
-            //console.log(r);
-            dispo -= 1;
-            $(".dispo").val(dispo);
         }
     });
 });
