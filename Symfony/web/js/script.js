6c3dd2a87a33d1c8acc1d30741aa3973bf8132bc
darkontes1 <darkontes@hotmail.fr>
2015-05-21 16:52:28 +0200

    mrow

--- Symfony/web/js/script.js
+++ Symfony/web/js/script.js
@@ -1,35 +1,25 @@
 $(document).on("click",".ajoutPanier",function(e){
     //console.log("aer");
     var button = $(this).attr("data-bdid");
-    //var panier = $("#panier").val();
-    var dispo = ($(".dispo").html());
     $.ajax({
         method:"POST",
         url:button,
-        data:{"dispo":dispo
-        },
         success:function(r){
             //console.log(r);
-            dispo -= 1;
-            $(this).prev(".dispo").css( "background", "yellow" );
-            //$(this).attr(".dispo").text($(this).attr(".dispo").text()-1);
+            $(this).parents(".dispo").val()
         }
     });
 });
 
 $(document).on("click",".panier_delete",function(e){
     console.log("aer");
-
     var button = $(this).attr("data-remove");
-    //var panier = $("#panier").val();
-    var dispo = ($(".dispo").val());
     $.ajax({
         method:"POST",
         url:button,
-        data:{
-        },
         success:function(r){
             //console.log(r);
+            $(this).parents(".apercu").hide("slow")
         }
     });
 });
